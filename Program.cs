383d3b3eb8301d3311cf6ea8313ede6fa1aa27aa
L. Medeiros <medeiros.serio@live.com>
2022-08-11 14:10:32 -0300

    Post de produtos

--- Program.cs
+++ Program.cs
@@ -10,13 +10,35 @@ app.MapGet("/AddHeader", (HttpResponse response) => {
     return new {Name= "Julia Vavlis", Status = "Muito Linda"};
 });
 
-app.MapPost("/saveproduct",(Produto produto) => {
-    return produto.Code + " | " + produto.Nome;
+app.MapPost("/saveproduct",(Product Product) => {
+    ProductRepository.Add(product);
+});
+
+app.MapPost("/getproduct/{code}",([FromRoute] string code) => {
+    var product = ProductRepository.GetBy(code);
 });
 
 app.Run();
 
-public class Produto {
+public class Product {
     public string Code { get; set; }
     public string Nome { get; set; }
 }
+
+public static class ProductRepository{
+    public static List<Product> Products { get; set; }
+
+    public static void Add(Product product) {
+        if(Products == null){
+            Products = new List<Product>();
+
+            Products.Add(product);
+        }
+    }
+
+    public static Product GetBy(string code){
+        return Products.First(p => p.Code == Code);
+    }
+}
+
+
